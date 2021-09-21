a41dedd8675b4fb5a3a81218f33ef85dd66d406f
Abdul Muhammad <abdulmuhammad331@gmail.com>
2021-09-20 21:26:01 -0700

    FEAT: added /abdul and .gitignore

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def hello_world(): 
     return "The Garage Trap House says hi! :)"
 
+@app.route("/abdul")
+def abdul():
+    return "Yo its ya boi Abdul"
+
 
 
 # the host value allows traffic from anywhere to run this 
