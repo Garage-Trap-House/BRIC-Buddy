d9f5aa289ec6aa18e39c1f8b9109854f228b75a7
Bryant Hong <bhong4059@gmail.com>
2021-09-20 17:29:47 -0700

    Feat: Wrote first route in HTTP API

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask 
+
+# make the flask app 
+app = Flask(__name__)
+
+# Bryant's HTTP request
+@app.route("/hello_world")
+def hello_world(): 
+    return "The Garage Trap House says hi :)"
+
+
+
+# the host value allows traffic from anywhere to run this 
+app.run(host = "0.0.0.0")
