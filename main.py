35df5d34510cb75335b158582fe629cfd8178aad
Bryant Hong <bhong4059@gmail.com>
2021-09-20 21:07:15 -0700

    Feat: Added first HTTP request

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Bryant's HTTP request
 @app.route("/hello_world")
 def hello_world(): 
-    return "The Garage Trap House says hi :)"
+    return "The Garage Trap House says hi! :)"
 
 
 
