4bbf548170a7b0d48e02f4329d06c10bee18c082
Bryant Hong <bhong4059@gmail.com>
2021-09-22 00:25:39 -0700

    Feat: Used Requests library to show functionality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask 
+import libraryTesting
 
 # make the flask app 
 app = Flask(__name__)
@@ -27,6 +28,11 @@ def abdul():
 @app.route("/lan")
 def lan():
     return "Hello! This is Lan :DDDD"
+
+@app.route("/requestTest")
+def reqTest():
+    return libraryTesting.requestsAttempt() 
+
 
 # the host value allows traffic from anywhere to run this 
 app.run(host = "0.0.0.0")
