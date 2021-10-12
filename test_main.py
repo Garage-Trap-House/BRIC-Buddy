c9479fd8a2d9bb537e36a6dfe60e4f40403b0bd6
Abdul Muhammad <abdulmuhammad331@gmail.com>
2021-10-11 21:18:28 -0700

    FIX: fixed a merge conflict

--- test_main.py
+++ test_main.py
@@ -6,11 +6,11 @@ import libraryTesting
 # Lan's test
 def test_mathAnswer():
     assert mathTest.mathAttempt(3,5) == 0
-import libraryTesting
 
 # Bryant's test
 def test_URLRequest(): 
     assert libraryTesting.requestsAttempt('https://www.youtube.com/') == 'it worked!'
 
+#Abdul's Test
 def test_abdulAttmeptPass():
     assert abdulAttempt(3,-1) == '-3.0'
