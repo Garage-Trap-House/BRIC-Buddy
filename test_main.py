3400f6180c242a06f21d3f901f378752bc0f920a
Abdul Muhammad <abdulmuhammad331@gmail.com>
2021-10-11 21:16:32 -0700

    FEAT: added test for abdul

--- test_main.py
+++ test_main.py
@@ -1,3 +1,7 @@
-import pytest
+from pytest import *
+from unittest.mock import patch
+from main import abdul, funcToTest
+from mathTest import abdulAttempt
 
-
+def test_abdulAttmeptPass():
+    assert abdulAttempt(3,-1) == '-3.0'
